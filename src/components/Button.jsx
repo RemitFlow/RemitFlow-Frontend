e7c6ad537af0b435a957e962d2ed4340f32bea5c
arisu6804 <arisu6804@gmail.com>
2026-06-12 17:11:10 +0530

    feat: add Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,28 @@
+import './Button.css'
+
+/**
+ * Reusable button.
+ * @param {object} props
+ * @param {'primary'|'secondary'|'ghost'} [props.variant] - visual style
+ * @param {boolean} [props.disabled]
+ * @param {Function} [props.onClick]
+ * @param {'button'|'submit'} [props.type]
+ */
+export default function Button({
+  children,
+  variant = 'primary',
+  disabled = false,
+  type = 'button',
+  onClick
+}) {
+  return (
+    <button
+      type={type}
+      className={`btn btn-${variant}`}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
+}
