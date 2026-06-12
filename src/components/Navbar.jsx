d6db2a34d2138c8162772789a2ffb1ab80ab110e
arisu6804 <arisu6804@gmail.com>
2026-06-12 17:12:19 +0530

    feat: add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import { NavLink, Link } from 'react-router-dom'
+import WalletButton from './WalletButton.jsx'
+import './Navbar.css'
+
+/**
+ * Top navigation bar with links and the wallet connect button.
+ */
+export default function Navbar() {
+  return (
+    <header className="navbar">
+      <Link to="/" className="navbar-brand">
+        <span className="navbar-logo">✦</span>
+        RemitFlow
+      </Link>
+
+      <nav className="navbar-links">
+        <NavLink to="/" end className="navbar-link">
+          Home
+        </NavLink>
+        <NavLink to="/send" className="navbar-link">
+          Send Money
+        </NavLink>
+        <NavLink to="/transfers" className="navbar-link">
+          Transfers
+        </NavLink>
+      </nav>
+
+      <div className="navbar-actions">
+        <WalletButton />
+      </div>
+    </header>
+  )
+}
